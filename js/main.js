1d06f344616fca269112820197b6089a2b50d731
JustZoB <justzofb@gmail.com>
2020-05-24 12:03:47 +0600

    Adapt slider, change animation slider, trying to pixelperfect :D

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
+const buttonsImg = document.querySelector(".main__aperti__img");
 const openButton = document.querySelector(".main__aperti__img__button__open");
 const closeButon = document.querySelector(".main__aperti__img__button__close");
 const aperti = document.querySelector(".main__aperti");
 const text = document.querySelector(".main__aperti__text");
+const sliderDots = document.querySelector(".slider__dots");
+
+aperti.style.transform = "translateY(" + text.clientHeight + "px)";
 
 openButton.onclick = function() {
+    buttonsImg.classList.add("active");
     openButton.style.display = "none";
     closeButon.style.display = "unset";
-    let start = 175;
+    sliderDots.style.zIndex = "0";
+    let start = text.clientHeight, end = 0;
 
     let showText = setInterval(function() {
         start -= 5;
-        if (start <= 0) {
+        if (start <= end) {
             clearInterval(showText);
         }
         aperti.style.transform = "translateY(" + start + "px)";
@@ -18,17 +24,25 @@ openButton.onclick = function() {
 }
 
 closeButon.onclick = function() {
+    buttonsImg.classList.remove("active");
     closeButon.style.display = "none";
     openButton.style.display = "unset";
-    let start = 0;
+    let start = 0, end = text.clientHeight;
 
     let showText = setInterval(function() {
         start += 5;
-        if (start >= 175) {
+        if (start >= end) {
+            sliderDots.style.zIndex = "2";
             clearInterval(showText);
         }
         aperti.style.transform = "translateY(" + start + "px)";
     }, 5);
+}
+
+window.onresize = function() {
+    if (buttonsImg.classList == "main__aperti__img") {
+        aperti.style.transform = "translateY(" + text.clientHeight + "px)";
+    }
 }
 
 const dropDownElem = document.querySelector(".dropdownmenu");
